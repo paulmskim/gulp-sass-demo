0b357fa815d08c04e794c2d1f233281092276f0c
paulmskim <kim.paul.ms@gmail.com>
2017-02-04 02:04:49 +0800

    add gulp-plumber, gulp-util, gulp-autoprefixer, gulp-clean-css, and gulp-rename to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    importer = require('node-sass-globbing');
+    importer = require('node-sass-globbing'),
+    plumber = require('gulp-plumber'),
+    util = require('gulp-util'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    log = util.log;
 
 var sass_config = {
   style: 'expanded',
@@ -16,11 +22,18 @@ gulp.task('default', function() {
 });
 
 gulp.task('sass', function() {
+  log('Generate CSS files ' + (new Date()).toString());
   gulp.src('sass/**/*.scss')
+    .pipe(plumber())
     .pipe(sass(sass_config))
+    .pipe(autoprefixer({browsers: ['last 2 versions', 'safari 5', 'ie 9']}))
+    .pipe(gulp.dest('css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleancss())
     .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch:sass', function() {
+  log('Watching scss files for modifications');
   gulp.watch('sass/**/*.scss', ['sass']);
 });
