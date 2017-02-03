c5112307be115d9de69e55ad74cc5bd8e478b68b
paulmskim <kim.paul.ms@gmail.com>
2017-02-04 01:37:56 +0800

    add gulp-sass, add sass tasks to gulpfile, add sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // do nothing
+});
+
+gulp.task('sass', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch:sass', function() {
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
